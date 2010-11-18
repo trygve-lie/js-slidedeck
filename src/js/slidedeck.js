7afba9d42481bb45912422b5bd0f32133b4b4ee1
Trygve Lie <git@trygve-lie.com>
2010-11-18 20:42:32 +0100

    Simplified dom walker. Added "almost at the end" and "almost at the beginning" value at the next / previous methods. Added default values on the styles.

--- src/js/slidedeck.js
+++ src/js/slidedeck.js
@@ -1,7 +1,6 @@
 var SlideDeck = (function () {
 
 
-
         // Set CSS on a deck
 
         var setStyle = function (element, index, start, styles) {
@@ -21,81 +20,94 @@ var SlideDeck = (function () {
         };
 
 
-
         // Find all first level children in the slidedeck, push them in a stack and apply css styles
     
         var readFirstLevelChildren = function (element, start, styles) {
 
             var children = [];
 
-            // TODO: A better recursive function: https://developer.mozilla.org/en/JavaScript/Reference/Functions_and_function_scope/arguments/callee
-
-            var walker = function (currentElement) {
+            var domWalker = function (currentElement) {
                 if (currentElement.nodeType === 1) {
                     children.push(currentElement);
                     setStyle(currentElement, children.length, start, styles);
                 }
                 var nextElement = currentElement.nextSibling;
                 if (nextElement !== null) {
-                    walker(nextElement);
+                    arguments.callee(nextElement);
                 }
-            };
-
-            walker(element.firstChild);
+            }(element.firstChild);
 
             return children;
         };
 
 
-
         // The Deck object
 
-        var Deck = function (start, elements, styles) {
+        var Deck = function (elements, start, styles) {
 
-            this.styles   = styles;
-            this.decks    = readFirstLevelChildren(elements.deck, start, styles);
+            this.styles   = styles || {past : 'past', future : 'future', current : 'current'};
             this.index    = (start - 1);
-
+            this.decks    = readFirstLevelChildren(elements.deck, start, this.styles);
         };
-
 
 
         Deck.prototype = {
 
+
+            // Move to the next deck in the slidedeck
+            
             next : function (callback) {
 
-                if (this.index < (this.decks.length - 1)) {
+                var obj = this;
+
+                if (obj.index < (obj.decks.length - 1)) {
+
+                    obj.decks[obj.index].className = obj.styles.past;
+                    obj.index = obj.index + 1;
+                    obj.decks[obj.index].className = obj.styles.current;
 
                     if (callback) {
-                        callback(this.index, this.decks.length);
+                        callback(obj.index === (obj.decks.length - 1));
                     }
 
-                    this.decks[this.index].className = this.styles.past;
-                    this.index = this.index + 1;
-                    this.decks[this.index].className = this.styles.current;
                 }
 
             },
 
+
+            // Move to the previous deck in the slidedeck
+
             previous : function (callback) {
 
-                if (this.index > 0) {
+                var obj = this;
+
+                if (obj.index > 0) {
+
+                    obj.decks[obj.index].className = obj.styles.future;
+                    obj.index = obj.index - 1;
+                    obj.decks[obj.index].className = obj.styles.current;
 
                     if (callback) {
-                        callback(this.index, this.decks.length);
+                        callback((obj.index - 1) === 0);                            // Calculate "almost at the beginning"
                     }
 
-                    this.decks[this.index].className = this.styles.future;
-                    this.index = this.index - 1;
-                    this.decks[this.index].className = this.styles.current;
                 }
                 
             },
 
-            append : function(element) {
 
-                this.decks.push(element);
-                setStyle(element, (this.decks.length - 1), this.index, this.styles);
+            // Append a new deck to the slidedeck
+
+            append : function (element, callback) {
+
+                var obj = this;
+
+                obj.decks.push(element);
+                setStyle(element, (obj.decks.length - 1), obj.index, obj.styles);   // Calculate "almost at the end"
+
+                if (callback) {
+                    callback();
+                }
 
             }
             
